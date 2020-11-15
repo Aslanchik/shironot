a2a7710eae2e9ffd7ad1f84157d1247db6a441a5
Aslan Badalov <aslanchikgw2@hotmail.com>
2020-11-15 10:02:52 +0200

    added main song and lyrics

--- src/components/SongContainer.jsx
+++ src/components/SongContainer.jsx
@@ -1,5 +1,7 @@
 import * as React from 'react';
 import M from "materialize-css/dist/js/materialize.min.js";
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 const SongContainer = () => {
 
@@ -10,10 +12,13 @@ const SongContainer = () => {
         <div className="container section">
             <div className="card">
     <div className="card-content">
-      <p className="card-title right">'עטור הוודג</p>
-        <button className="btn-floating btn-large waves-effect waves-light" title="הקשיבו הקשיבו"><i className="material-icons volumeIcon">volume_up</i></button>
-        <br/>
-      <p className="right">נכתב במקור ע"י אריק אינשטיין וב12 לנובמבר נרצח ע"י ליבי וגילי</p>
+      <div className="card-title center">'עטור הוודג</div>
+      <p className="center">נכתב במקור ע"י אברהם חלפי והולחן ע"י יוני רכטר, וב12 לנובמבר נרצח ע"י ליבי וגילי</p>
+      <br/>
+      <AudioPlayer
+    src="עטור הוודג.mp3"
+
+  />
     </div>
     <div className="card-tabs">
       <ul className="tabs tabs-fixed-width">
@@ -22,27 +27,52 @@ const SongContainer = () => {
       </ul>
     </div>
     <div className="card-content grey lighten-5">
-      <div id="test4">מילות השיר המקורי</div>
-      <div id="test5" className="right">עטור הוודג' רוז גולד
-(וואלה לא זוכר מה הוא כתב)
-הוודג' מתחרז עם עיניים ואור
-(אנ'לא סגור למה)
-
-הבייבידול שלך צמרירי ורך
-נודר - תענוג לעל ליל
-לא הייתי רוצה להיות לך בפרינדזון
-אל תקראי לי יא אח
-חולם עלייך בתולה וקדושה
-יא עיוני איזו אישה!
-
-מה נפלו פנייך מאמי?
-מדוע את משחקת אותה?
-חדל ללרלר על קרוב על רחוק
-ואני, שלא פעם עושה לך סטוק
-מתנחלת נפשי בביתך כבר שבועיים
-באלוהים שבועיים
-אם ממני את נפרדת
-אני מתאבד.</div>
+      <blockquote id="test4" className="center"><p>עטור מצחך זהב שחור</p>
+<p>(אינני זוכר אם כתבו כך בשיר)</p>
+<p>מצחך מתחרז עם עיניים ואור</p>
+<p>(אינני זוכר אם חרזו כך בשיר)</p>
+<p>אך למי שתהיי</p>
+<p>חייו מלאי שיר</p>
+<br/>
+<p>חלוקך הורוד צמרירי ורך</p>
+<p>את בו מתעטפת תמיד לעת ליל</p>
+<p>לא הייתי רוצה להיות לך אח</p>
+<p>לא נזיר מתפלל לדמותו של מלאך</p>
+<p>ורואה חלומות עגומים של קדושה</p>
+<p>...ולמולו את אישה</p>
+<br/>
+<p>את אוהבת להיות</p>
+<p>עצובה ושותקת</p>
+<p>להקשיב לסיפור על קרוב על רחוק</p>
+<p>ואני, שלא פעם אביט בך בשקט</p>
+<p>אין קול ודברים</p>
+<p>שוכח הכל על אודות אחרים</p>
+<p>שוכנת נפשי בין כתלי ביתך</p>
+<p>ושבויה בין כתלייך</p>
+<p>ממני נפרדת</p>
+<p>.עת אני בגופי נפרד ממך</p>
+</blockquote>
+      <blockquote id="test5" className="center"><p>עטור הוודג' רוז גולד</p>
+<p>(וואלה לא זוכר מה הוא כתב)</p>
+<p>הוודג' מתחרז עם עיניים ואור</p>
+<p>(אנ'לא סגור למה)</p>
+<br/>
+<p>הבייבידול שלך צמרירי ורך</p>
+<p>נודר - תענוג לעל ליל</p>
+<p>לא הייתי רוצה להיות לך בפרינדזון</p>
+<p>אל תקראי לי יא אח</p>
+<p>חולם עלייך בתולה וקדושה</p>
+<p>יא עיוני איזו אישה!</p>
+<br/>
+<p>מה נפלו פנייך מאמי?</p>
+<p>מדוע את משחקת אותה?</p>
+<p>חדל ללרלר על קרוב על רחוק</p>
+<p>ואני, שלא פעם עושה לך סטוק</p>
+<p>מתנחלת נפשי בביתך כבר שבועיים</p>
+<p>באלוהים שבועיים</p>
+<p>אם ממני את נפרדת</p>
+<p>.אני מתאבד</p>
+</blockquote>
 
     </div>
   </div>
